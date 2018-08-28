f12356a4e571b806aa2e4f52086112df9552840d
Ajil Raju <ajilraju01@gmail.com>
2018-08-28 18:56:26 +0530

    removing extra indentation

--- calculate.h
+++ calculate.h
@@ -6,12 +6,12 @@
 #ifndef __CALCULATE_H__
 #define __CALCULATE_H__
 
-	void difference(void);
-	void produit(void);
-	void quotient(void);
-	void racine_carre(void);
-	void somme(void);
-	void menu_principal(void);
+void difference(void);
+void produit(void);
+void quotient(void);
+void racine_carre(void);
+void somme(void);
+void menu_principal(void);
 
 
 #endif
